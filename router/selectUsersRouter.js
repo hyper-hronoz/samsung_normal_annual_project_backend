a7830d911036afe8727bad28eadc1926db525522
Vladislav <vlad_moryakov_prog@mail.ru>
2021-06-13 00:09:51 +0300

    рефакторинг и ревью

--- router/selectUsersRouter.js
+++ router/selectUsersRouter.js
@@ -3,7 +3,6 @@ const controller = require("../controllers/selectUserController")
 const authMiddlewaree = require("../middlewaree/authMiddlewaree")
 
 router.get("/user", authMiddlewaree, controller.getRandomUser)
-// router.put("/user", authMiddlewaree, controller.updateUserData)
 // router.post("/upload", authMiddlewaree, controller.upload)
 
 module.exports = router
